0f04899414ebae4fc9a9b302b437f0a273fda707
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-11-13 10:49:09 +0530

    Update Registrationdb.java

--- src/main/java/dbconnection/Registrationdb.java
+++ src/main/java/dbconnection/Registrationdb.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class Registrationdb {
 	
 	private static String Driver="com.mysql.cj.jdbc.Driver";
-	private static String url="jdbc:mysql://localhost:3306/studentdb";
-	private static String username="root";
-	private static String password="Govind123@.";
+	private static String url="jdbc:mysql://db:3306/student";
+	private static String username="govind";
+	private static String password="govind123";
 	private static Connection conn=null;
 	public static Connection getconn()
 	{
